9b85abdc8cf1fad0534353c076614927e823b724
ZeyadMNagi <zeyadnagi421@gmail.com>
2023-10-30 21:55:45 +0200

    organize P.1

---
+++ two-player/the main/js/offset.js
@@ -0,0 +1,113 @@
+//offset for the roof background
+
+if (player_use.name === "Evil wizard" && background_use.name === "roof") {
+  player_use.offset.y = 270;
+}
+if (enemy_use.name === "Evil wizard" && background_use.name === "roof") {
+  enemy_use.offset.y = 270;
+}
+if (player_use.name === "Knight" && background_use.name === "roof") {
+  player_use.offset.y = 100;
+}
+if (enemy_use.name === "Knight" && background_use.name === "roof") {
+  enemy_use.offset.y = 100;
+}
+if (player_use.name === "king" && background_use.name === "roof") {
+  player_use.offset.y = 90;
+}
+if (enemy_use.name === "king" && background_use.name === "roof") {
+  enemy_use.offset.y = 90;
+}
+if (player_use.name === "samurai" && background_use.name === "roof") {
+  player_use.offset.y = 150;
+}
+if (enemy_use.name === "samurai" && background_use.name === "roof") {
+  enemy_use.offset.y = 150;
+}
+if (player_use.name === "warrior" && background_use.name === "roof") {
+  player_use.offset.y = 130;
+}
+if (enemy_use.name === "warrior" && background_use.name === "roof") {
+  enemy_use.offset.y = 130;
+}
+if (player_use.name === "wizard" && background_use.name === "roof") {
+  player_use.offset.y = 90;
+}
+if (enemy_use.name === "wizard" && background_use.name === "roof") {
+  enemy_use.offset.y = 90;
+}
+if (player_use.name === "goblin" && background_use.name === "roof") {
+  player_use.offset.y = 90;
+}
+if (enemy_use.name === "goblin" && background_use.name === "roof") {
+  enemy_use.offset.y = 90;
+}
+if (player_use.name === "skeleton" && background_use.name === "roof") {
+  player_use.offset.y = 90;
+}
+if (enemy_use.name === "skeleton" && background_use.name === "roof") {
+  enemy_use.offset.y = 90;
+}
+if (player_use.name === "fire wizard" && background_use.name === "roof") {
+  player_use.offset.y = 90;
+}
+if (enemy_use.name === "fire wizard" && background_use.name === "roof") {
+  enemy_use.offset.y = 90;
+}
+
+//the offset for the forest background
+
+if (player_use.name === "Evil wizard" && background_use.name === "forest") {
+  player_use.offset.y = 310;
+}
+if (enemy_use.name === "Evil wizard" && background_use.name === "forest") {
+  enemy_use.offset.y = 310;
+}
+if (player_use.name === "Knight" && background_use.name === "forest") {
+  player_use.offset.y = 110;
+}
+if (enemy_use.name === "Knight" && background_use.name === "forest") {
+  enemy_use.offset.y = 110;
+}
+if (player_use.name === "king" && background_use.name === "forest") {
+  player_use.offset.y = 110;
+}
+if (enemy_use.name === "king" && background_use.name === "forest") {
+  enemy_use.offset.y = 110;
+}
+if (player_use.name === "samurai" && background_use.name === "forest") {
+  player_use.offset.y = 170;
+}
+if (enemy_use.name === "samurai" && background_use.name === "forest") {
+  enemy_use.offset.y = 170;
+}
+if (player_use.name === "warrior" && background_use.name === "forest") {
+  player_use.offset.y = 150;
+}
+if (enemy_use.name === "warrior" && background_use.name === "forest") {
+  enemy_use.offset.y = 150;
+}
+if (player_use.name === "wizard" && background_use.name === "forest") {
+  player_use.offset.y = 110;
+}
+if (enemy_use.name === "wizard" && background_use.name === "forest") {
+  enemy_use.offset.y = 110;
+}
+if (player_use.name === "goblin" && background_use.name === "forest") {
+  player_use.offset.y = 110;
+}
+if (enemy_use.name === "goblin" && background_use.name === "forest") {
+  enemy_use.offset.y = 110;
+}
+if (player_use.name === "skeleton" && background_use.name === "forest") {
+  player_use.offset.y = 110;
+}
+if (enemy_use.name === "skeleton" && background_use.name === "forest") {
+  enemy_use.offset.y = 110;
+}
+if (player_use.name === "fire wizard" && background_use.name === "forest") {
+  player_use.offset.y = 110;
+}
+if (enemy_use.name === "fire wizard" && background_use.name === "forest") {
+  enemy_use.offset.y = 110;
+}
